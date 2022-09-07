a38acba47066fe2d8b87cbd9f70b6e090a5ed97a
Coders Key <designer.afride@gmail.com>
2022-09-07 10:53:33 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center">
+        <div>
+            <h3 className="text-3xl font-bold underline text-center text-clifford my-6"> Meet the them</h3>
+            <div className="flex flex-row mb-10 relative">
+                <div>
+                    <img src="https://randomuser.me/api/portraits/women/72.jpg" alt="" />
+                    <p className="text-xs">Monet Goode</p>
+                    <p className="text-xs">Founder</p>
+                </div>
+                <div className="absolute top-10 left-24">
+                    <img src="https://randomuser.me/api/portraits/men/75.jpg"  alt="" />
+                    <p className="text-xs">Audrius Linkus</p>
+                    <p className="text-xs">Head of interactive</p>
+                </div>
+            </div>
+            <div className="flex flex-row relative">
+                <div>
+                    <img src="https://randomuser.me/api/portraits/men/70.jpg"  alt="" />
+                    <p className="text-xs">Manvydas Kugis</p>
+                    <p className="text-xs">Digital director</p>
+                </div>
+                <div className="absolute top-10 left-24">
+                    <img src="https://randomuser.me/api/portraits/women/75.jpg" alt="" />
+                    <p className="text-xs">Nicole Wiegand</p>
+                    <p className="text-xs">Project manager</p>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
