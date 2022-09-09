ee833caaa91c2a476b45154ae970f7b433d09107
Coders Key <designer.afride@gmail.com>
2022-09-09 11:02:58 +0600

    animation added

--- src/pages/Banner/Banner.js
+++ src/pages/Banner/Banner.js
@@ -5,12 +5,14 @@ const Banner = () => {
       return (
             <div id="intro">
                   <div className="jumbotron text-center">
-                        <h2>TECHBOOM</h2>
-                        <div class="animation">
-                              <div class="cloud1" id="animation"><img src="https://techboom.ai/images/cloud1%20(1).png"/></div>
-                              <div class="cloud2" id="animation"><img src="https://techboom.ai/images/cloud2%20(1).png"/></div>
-                              <div class="cloud3" id="animation"><img src="https://techboom.ai/images/cloud1%20(1).png"/></div>
-                              <div class="cloud4" id="animation"><img src="https://techboom.ai/images/cloud2%20(1).png"/></div>
+                        <div className="jumbotron-container">
+                              <h2>TECHBOOM</h2>
+                              <div class="animation">
+                                    <div class="cloud1" id="animation"><img src="https://techboom.ai/images/cloud1%20(1).png"/></div>
+                                    <div class="cloud2" id="animation"><img src="https://techboom.ai/images/cloud2%20(1).png"/></div>
+                                    <div class="cloud3" id="animation"><img src="https://techboom.ai/images/cloud1%20(1).png"/></div>
+                                    <div class="cloud4" id="animation"><img src="https://techboom.ai/images/cloud2%20(1).png"/></div>
+                              </div>
                         </div>
                   </div>
             </div>
